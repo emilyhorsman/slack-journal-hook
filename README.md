3544bfe1b8d41b77648f3388ca902fa9b1b18490
Emily Horsman <me@emilyhorsman.com>
2015-05-13 14:23:50 -0400

    Initial project.

---
+++ README.md
@@ -0,0 +1,10 @@
+#### Purpose
+
+I've been keeping a journal of text-based entries in a Dropbox folder ever since moving away from Day One. I had been wanting a way to note key things in each entry so that I could review them later. Initially, I wrote a script to go through the entries and print out highlights from the last few days. Highlights were any line in the journal starting with a `Highlight: ` prompt. Remembering to run the script and read through everything was a pain though. I stumbled across the idea of using a personal Slack team as a stream of consciousness/knowledge archive and organizer. A file monitor that messaged these highlights into my personal Slack channels seemed perfect.
+
+#### Journal Usage
+
+```
+Highlight: This highlight goes to the default channel specified in config.py.
+Highlight: #random This highlight goes to the #random channel.
+```
---
+++ main.py
@@ -0,0 +1,78 @@
+import json
+import time
+from hashlib import md5
+from os.path import join, getmtime, isfile
+from os import listdir
+
+import requests
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+import config
+
+def log_string(msg, channel):
+    return md5(channel + msg).digest()
+
+def log_message(msg, channel):
+    with open("archive", "a") as f:
+        f.write(log_string(msg, channel) + "\n")
+
+def in_log(msg, channel):
+    if not isfile("archive"):
+        return False
+
+    t = log_string(msg, channel)
+    with open("archive", "r") as f:
+        for line in f:
+            if line.strip() == t:
+                return True
+
+    return False
+
+def send_message(msg, channel=config.default_channel):
+    payload = {
+        "text": msg,
+        "channel": channel,
+        "icon_emoji": config.bot_emoji,
+        "username": config.bot_username
+        }
+    r = requests.post(config.webhook_url, data=json.dumps(payload))
+
+def process_journal_line(line):
+    if line.find(config.prompt) != 0:
+        return
+
+    msg = line[len(config.prompt):].strip()
+    channel = config.default_channel
+    if msg.find("#") == 0: # See if a channel has been specified.
+        channel, msg = msg.split(" ", 1)
+
+    if not in_log(msg, channel):
+        log_message(msg, channel)
+        send_message(msg, channel)
+
+def process(path):
+    with open(path, "r") as f:
+        for line in f: process_journal_line(line)
+
+class JournalHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.is_directory:
+            # FSEvents in OS X only returns the directory when a file is
+            # modified, so get the most recently modified folder.
+            files = [ join(event.src_path, f) for f in listdir(event.src_path) ]
+            modified = max(files, key=getmtime)
+            process(modified)
+
+if __name__ == "__main__":
+    event_handler = JournalHandler()
+    observer = Observer()
+    observer.schedule(event_handler, config.journal_entry_path)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
